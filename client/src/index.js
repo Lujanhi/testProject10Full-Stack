975749bb8f543c79023d6fb42cd8ab058aaaa3ce
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-09-20 14:46:54 -0400

    save 9

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './global.css';
+
+//Provider is importing the component Provider from the context.js file
+import { Provider } from './components/Context';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Add the components into the HTML page
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
